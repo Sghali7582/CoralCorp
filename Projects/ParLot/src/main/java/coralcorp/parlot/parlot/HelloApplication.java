b99ab87869345bde77578b5b83585e64ebebe7c7
CoralCorp <90718965+CoralCorp@users.noreply.github.com>
2021-11-17 18:23:35 -0600

    Delete Parlot try
    delete the folder that contains malconfigurations

--- Projects/ParLot/src/main/java/coralcorp/parlot/parlot/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package coralcorp.parlot.parlot;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
